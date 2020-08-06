2923ada6fe24f2af6d70d8585bd5c5566ff49bb7
Frank Wagner <frank2568@gmail.com>
2020-08-06 12:07:56 +0200

    updated documentation

--- src/Hosuto/Modules/WebModule.cs
+++ src/Hosuto/Modules/WebModule.cs
@@ -1,5 +1,12 @@
 ﻿namespace Dbosoft.Hosuto.Modules
 {
+    /// <summary>
+    /// Web module is the abstract base class of a module that runs a aspnetcore host
+    /// It replaces the Startup class and can be used almost like a startup class.
+    /// See <see cref="IModule"/> for conventions.
+    /// In addition to the ConfigureServices you should also add a Configure method to the
+    /// web module.   
+    /// </summary>
     public abstract class WebModule : IModule
     {
         public abstract string Path { get; }
