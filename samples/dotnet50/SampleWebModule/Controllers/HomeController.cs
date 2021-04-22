95719e684aeae98a537d43d479e0c998bf1c804f
Frank Wagner <frank2568@gmail.com>
2021-04-22 15:02:34 +0200

    added .net 5 support (#22)

---
+++ samples/dotnet50/SampleWebModule/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Dbosoft.Hosuto.Sample.Models;
+using Dbosoft.Hosuto.Samples;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Dbosoft.Hosuto.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMessageDispatcher _messageDispatcher;
+
+        public HomeController(ILogger<HomeController> logger, IMessageDispatcher messageDispatcher)
+        {
+            _logger = logger;
+            _messageDispatcher = messageDispatcher;
+        }
+
+        public IActionResult Index()
+        {
+            _messageDispatcher.SendMessage(this, "Someone has opened Home page!! Good start!");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _messageDispatcher.SendMessage(this, "Someone has opened Privacy page!! Great!");
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ samples/dotnet50/SampleWebModule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dbosoft.Hosuto.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
