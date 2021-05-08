1d16079a729a9b7d7bdf28dee409527fcb8937cd
Frank Wagner <frank2568@gmail.com>
2021-05-08 19:16:27 +0200

    added tests for configuration and logger (#23)

--- test/Hosuto.Hosting.Tests/Modules/Hosting/ModuleBuildingTests.cs
+++ test/Hosuto.Hosting.Tests/Modules/Hosting/ModuleBuildingTests.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
 using Dbosoft.Hosuto.Modules;
 using Dbosoft.Hosuto.Modules.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Hosuto.Hosting.Tests.Modules.Hosting
@@ -25,6 +29,53 @@ namespace Hosuto.Hosting.Tests.Modules.Hosting
         }
 
         [Fact]
+        public void Build_module_has_configuration_from_host()
+        {
+            var builder = ModulesHost.CreateDefaultBuilder();
+            builder.HostModule<SomeModule>();
+            builder.ConfigureAppConfiguration(c => 
+                c.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    ["test"] = "testValue"
+                }));
+
+            var host = builder.Build();
+
+            var module = host.Services.GetRequiredService<SomeModule>();
+            Assert.Equal("testValue", module.Configuration["test"]);
+        }
+
+        [Fact]
+        public void Build_module_has_logger_from_host()
+        {
+            var builder = ModulesHost.CreateDefaultBuilder();
+
+            var loggerMock = new Mock<ILogger>();
+            loggerMock.Setup(x => x.Log(LogLevel.Trace,
+                        It.IsAny<EventId>(), null, It.IsAny<Exception>(), 
+                        It.IsAny<Func<object, Exception, string>>())).Verifiable();
+
+                var logProviderMoq = new Mock<ILoggerProvider>();
+            logProviderMoq.Setup(x => 
+                x.CreateLogger(It.IsAny<string>())).Returns(loggerMock.Object);
+
+            builder.HostModule<SomeModule>();
+
+            builder.ConfigureLogging(lb =>
+                lb.SetMinimumLevel(LogLevel.Trace)
+                .AddProvider(logProviderMoq.Object));
+
+            var host = builder.Build();
+
+            var moduleHost = host.Services.GetRequiredService<IModuleHost<SomeModule>>();
+            var loggerFactory = moduleHost.Services.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("dummy");
+            logger.Log<object>(LogLevel.Trace, new EventId(0), null, null, (s, e) => "trace message");
+
+            loggerMock.Verify();
+        }
+
+        [Fact]
         public void Multiple_modules_can_be_bootstrapped_and_see_each_other()
         {
             var builder = ModulesHost.CreateDefaultBuilder();
@@ -41,12 +92,15 @@ namespace Hosuto.Hosting.Tests.Modules.Hosting
         {
             public string Name => "I'm a module";
             public string Environment { get; private set; }
+            public IConfiguration Configuration { get; private set; }
+
 
 #pragma warning disable 618
-            public void ConfigureServices(IServiceCollection services, IHostingEnvironment environment)
+            public void ConfigureServices(IServiceCollection services, IHostingEnvironment environment, IConfiguration configuration)
 #pragma warning restore 618
             {
                 Environment = environment.EnvironmentName;
+                Configuration = configuration;
             }
 
         }
